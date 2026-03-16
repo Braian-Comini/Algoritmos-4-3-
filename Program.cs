6b282671eb0f20f0fa156a284fcb110cf129dbb3
CoMi-Com <braiancomini14et32@gmail.com>
2026-03-16 15:25:46 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+class Programa
+{
+    static void Main()
+    {
+        int alumnos;
+        int computadoras;
+
+        Console.WriteLine("Indica la cantidad de alumnos: ");
+        while (!int.TryParse(Console.ReadLine(), out alumnos))
+        {
+            Console.WriteLine("Entrada no válida. Por favor, ingresá un número entero.");
+        }
+
+        Console.WriteLine("Indica la cantidad de computadoras: ");
+        while (!int.TryParse(Console.ReadLine(), out computadoras))
+        {
+            Console.WriteLine("Entrada no válida. Por favor, ingresá un número entero.");
+        }
+
+        double promedio = (double)alumnos / computadoras;
+
+        if (alumnos > computadoras)
+        {
+            Console.WriteLine("Faltan equipos");
+        }
+        else if (alumnos >= computadoras)
+        {
+            Console.WriteLine("Equipos suficientes");
+        }
+
+        Console.WriteLine($"El promedio de alumnos por computadoras es: {promedio}");
+
+
+
+    }
+}

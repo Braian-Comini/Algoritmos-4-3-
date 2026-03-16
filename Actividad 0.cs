9cfbdbdfa395b8c05580a685e14631c5d52cb476
CoMi-Com <braiancomini14et32@gmail.com>
2026-03-16 15:09:39 -0300

    Add files via upload

---
+++ Actividad 0.cs
@@ -0,0 +1,50 @@
+﻿using System;
+class Programa
+{
+    static void Main()
+    {
+        int alumnos;
+        int computadoras;
+
+        Console.WriteLine("Indica la cantidad de alumnos: ");
+        while (!int.TryParse(Console.ReadLine(), out alumnos))
+        {
+            Console.WriteLine("Entrada no válida. Por favor, ingresá un número entero.");
+        }
+
+        Console.WriteLine("Indica la cantidad de computadoras: ");
+        while (!int.TryParse(Console.ReadLine(), out computadoras))
+        {
+            Console.WriteLine("Entrada no válida. Por favor, ingresá un número entero.");
+        }
+
+        double promedio = (double)alumnos / computadoras;
+
+        if (alumnos > computadoras)
+        {
+            Console.WriteLine("Faltan equipos");
+        }
+        else if (computadoras >= alumnos)
+        {
+            Console.WriteLine("Equipos suficientes");
+        }
+
+
+
+
+    }
+}
+/*
+class Pasaje
+{
+    static void Main()
+    {
+        int A = 3;
+        int B = 4;
+        int C = A + B;
+        Console.WriteLine("Resultado de A + B: ");
+        Console.WriteLine(C);
+    }
+    
+}
+*/

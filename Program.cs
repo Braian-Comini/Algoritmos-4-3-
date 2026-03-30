e16fd3599a12a4e91fe57f0e83cda5367c14467f
CoMi-Com <braiancomini14et32@gmail.com>
2026-03-30 17:48:53 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class ConsoleApp_1
+{
+    static void Main()
+    {
+        /*
+        int a = 10;
+        int b = 25;
+        int c = 0;
+
+        Console.WriteLine($"Valores viejos A = {a} B = {b}.");
+
+        c = a;
+        a = b;
+        b = c;
+
+        Console.WriteLine($"Valores nuevos A = {a} B = {b}.");
+        */
+
+        int a = 10;
+        int b = 25;
+
+        Console.WriteLine($"Valores viejos A = {a} B = {b}.");
+
+        a = a + 15;
+        b = b - 15;
+
+        Console.WriteLine($"Valores nuevos A = {a} B = {b}.");
+
+    }
+}

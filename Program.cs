4fb55345646a6fc5a0cfa2f0d12f0259c9590dc6
CoMi-Com <braiancomini14et32@gmail.com>
2026-04-10 15:41:16 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-
-class ConsoleApp2 
+class ConsoleApp2
 {
     static void Main()
     {
         int[,] escuela = new int[2, 3];
-        int piso = escuela[0,0];
-        int laboratorio = escuela[0,0];
-        
+        int piso = 0
+        int laboratorio = 0
+
         for (int i = 0; i < escuela.GetLength(0); i++)
         {
             Console.WriteLine($"Ingrese alumnos del piso {i + 1}: ");
@@ -17,7 +16,7 @@ class ConsoleApp2
                 Console.WriteLine("Entrada no válida. Por favor, ingresá un número entero.");
                 floor = Console.ReadLine();
             }
-            for (int f = 0;f < escuela.GetLength(1);f++)
+            for (int f = 0; f < escuela.GetLength(1); f++)
             {
                 Console.WriteLine($"Ingrese alumnos del laboratorio {f + 1}: ");
                 string? lab = Console.ReadLine();
@@ -31,47 +30,37 @@ class ConsoleApp2
 
         //Ingresar piso y lab para consultar datos.
         //Usar tryparce para error EJ: si consulta piso 5 (no existe).
+        bool datos_validos = false;
 
-        for(int j = 0; j < escuela.GetLength(0); j++)
+        for (int a = 0;a < escuela.GetLength(0); a++)
         {
-            Console.WriteLine($"Ingrese el piso a consultar: ");
+            Console.WriteLine("Ingrese el piso a consultar: ");
             string? input = Console.ReadLine();
-            while(!int.TryParse(input, out piso))
+            while (!int.TryParse(input, out piso))
             {
-                Console.WriteLine("Entrada no válida. Por favor, ingresá un número entero.");
-                input = Console.ReadLine();
+                if(piso >= 0 && piso < a) 
+                {
+                    Console.WriteLine(escuela);
+                    continue;
+                }
             }
-            for(int k = 0;k < escuela.GetLength(1); k++)
+            for(int b = 0; b < escuela.GetLength(1); b++)
             {
                 Console.WriteLine("Ingrese el laboratorio a consultar: ");
                 string? input2 = Console.ReadLine();
-                while (!int.TryParse(input2, out laboratorio))
+                while(!int.TryParse(input2, out laboratorio))
                 {
-                    Console.WriteLine("Entrada no válida. Por favor, ingresá un número entero.");
-                    input2 = Console.ReadLine();
+                    Console.WriteLine(escuela);
+                    datos_validos = true;
                 }
-                string a = input;
-                string b = input2;
-                if(a == "1")
-                {
-                 
-                    if(b == "1")
-                    {
-
-                    }
-                    
-                }else if(a == "2")
-                {
-
-                }
-
             }
 
-
+            }
         }
-
-
-
         
+
+
+
+
     }
 }

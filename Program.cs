dd7db72adb1c9df5260589661597b675bd2a7361
CoMi-Com <braiancomini14et32@gmail.com>
2026-04-27 17:29:11 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,121 +1,62 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 class ConsoleApp2
 {
     static void Main()
     {
         int[,] escuela = new int[2, 3];
-        int piso = 0;
-        int laboratorio = 0;
 
         for (int i = 0; i < escuela.GetLength(0); i++)
         {
-            Console.WriteLine($"Ingrese alumnos del piso {i + 1}: ");
-            string? floor = Console.ReadLine();
-            while (!int.TryParse(floor, out piso))
-            {
-                Console.WriteLine("Entrada no válida. Por favor, ingresá un número entero.");
-                floor = Console.ReadLine();
-                piso = i;
-                
-            }
             for (int f = 0; f < escuela.GetLength(1); f++)
             {
-                Console.WriteLine($"Ingrese alumnos del laboratorio {f + 1}: ");
-                string? lab = Console.ReadLine();
-                while (!int.TryParse(lab, out laboratorio))
+                Console.WriteLine($"Ingrese alumnos del piso {i + 1} laboratorio {f + 1}: ");
+                string? labs = Console.ReadLine();
+                int alumnos;
+                while (!int.TryParse(labs, out alumnos))
                 {
                     Console.WriteLine("Entrada no válida. Por favor, ingresá un número entero.");
-                    lab = Console.ReadLine();
-                    laboratorio = f;
-                   
+                    labs = Console.ReadLine();
+                    
+
                 }
+                
+                escuela[i,f] = alumnos;
             }
         }
-        Console.WriteLine($"Pisos: {piso}");
-        Console.WriteLine($"Labs: {laboratorio}");
-        Console.WriteLine($"Total escuela: {escuela}");
 
-        //Ingresar piso y lab para consultar datos.
-        //Usar tryparce para error EJ: si consulta piso 5 (no existe).
-        /*bool datos_validos = false;
-        int consulta = 0;
-        int consulta2 = 0;
-
-        Console.WriteLine("Ingrese piso a consultar: ");
-        string? input = Console.ReadLine();
-        if(int.TryParse(input, out consulta))
+        while (true)
         {
-            if(input == )
+            Console.WriteLine("Ingrese piso a consultar (1-2):");
+            string? p = Console.ReadLine();
+            Console.WriteLine("Ingrese el laboratorio a consultar (1-3):");
+            string? l = Console.ReadLine();
+
+            int piso, lab;
+
+
+            if (int.TryParse(p, out piso) && int.TryParse(l, out lab))
             {
 
+                if (piso >= 1 && piso <= 2 && lab >= 1 && lab <= 3)
+                {
+                    Console.WriteLine($"En el Piso {piso}, Lab {lab} hay: {escuela[piso - 1, lab - 1]} alumnos");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Piso o Laboratorio fuera de rango.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ingresar números enteros válidos.");
             }
         }
-        else
-        {
-            Console.WriteLine("Dato inválido, ingrese el piso correspondiente.");
-            input = Console.ReadLine();
-        }*/
+        int cantidad = 0;
+        int suma = escuela[0, 0] + escuela[0, 1] + escuela[0, 2];
+        cantidad = cantidad + suma;
+        Console.WriteLine($"En el primer piso son {cantidad} alumnos.");
         
-
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*for (int a = 0; a < 2; a++)
-        {
-            Console.WriteLine("Ingrese el piso a consultar: ");
-            string? input = Console.ReadLine();
-            while (!int.TryParse(input, out piso))
-            {
-                Console.WriteLine("Dato inválido, ingrese el piso correspondiente.");
-                input = Console.ReadLine();
-                piso = escuela.GetLength(0);
-                Console.WriteLine(escuela);
-                continue;
-                
-            }
-            for (int b = 0; b < 3; b++)
-            {
-                Console.WriteLine("Ingrese el laboratorio a consultar: ");
-                string? input2 = Console.ReadLine();
-                while (!int.TryParse(input2, out laboratorio))
-                {
-                    Console.WriteLine("Dato inválido, ingrese el laboratorio correspondiente.");
-                    input = Console.ReadLine();
-                    laboratorio = escuela.GetLength(1);
-                    Console.WriteLine(escuela);
-                    datos_validos = true;
-                    break;
-                 
-                }
-            }
-
-
-        }
-
-        Console.WriteLine($"Varialble datos_validos: {datos_validos} ");
-        Console.WriteLine($"Piso: {piso} ");
-        Console.WriteLine($"Laboratorio: {laboratorio} ");
-        */
